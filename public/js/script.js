65fea67090b1b66b015f7c0bff6d826e6eb3ab8e
Kevon Cooper <imkvncpr@gmail.com>
2024-03-04 21:44:04 -0500

    Final

--- public/js/script.js
+++ public/js/script.js
@@ -30,16 +30,29 @@ function findBook(){
                 wrapperDiv.appendChild(div);
                 // create h5 element for author
                 var author = document.createElement('h6');
-                author.innerHTML = book.items[i].volumeInfo.authors[i];
+                author.innerHTML =  '<b>Author:</b>' + ' ' + book.items[i].volumeInfo.authors;
                 div.appendChild(author);
                 // create paragraph for country
                 var country = document.createElement('p');
-                country.innerHTML = 'Country:' + ' ' + book.items[i].accessInfo.country;
+                country.innerHTML = '<b>Country:</b>' + ' ' + book.items[i].accessInfo.country;
                 div.appendChild(country);
-                // create element for discription
+                //create paragraph for pageCount
+                var pageCount = document.createElement('p');
+                pageCount.innerHTML = '<b>Page Count:</b>' + ' ' +  book.items[i].volumeInfo.pageCount;
+                div.appendChild(pageCount);
+                //create element for date
+                var publisherYear = document.createElement('p');
+                publisherYear.innerHTML = '<b>Year Published:</b>' + ' ' + book.items[i].volumeInfo.publishedDate;
+                div.appendChild(publisherYear);
+                // //create element for discription
                 var desc = document.createElement('p');
                 desc.innerHTML = book.items[i].volumeInfo.description;
                 div.appendChild(desc);
+                //create a tag to target link
+                var link = document.createElement('a');
+                link.innerHTML = '<b>View more</b>';
+                link.href = book.items[i].volumeInfo.previewLink;
+                div.appendChild(link);
                 // create hr to separate every books info
                 var line = document.createElement('hr');
                 // Make every elements as children element of bookResult
